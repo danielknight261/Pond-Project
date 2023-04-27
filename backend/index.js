791d8b7a1ef16a8abe8390f80e94d94051010f71
danielknight261 <100095189+danielknight261@users.noreply.github.com>
2023-04-27 12:29:11 +0100

    backend server running

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,42 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const port = process.env.port
-const cors = require("cors")
-const cloudinary = require("./cloudinary/cloudinary")
+const port = process.env.PORT || 8001;
+const cors = require("cors");
+const cloudinary = require("./cloudinary/cloudinary");
 
 app.use(cors());
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({extended: true , limit: '50mb'}));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+
+app.get("/", (req, res) => {
+  res.send("welcome");
+});
+
+app.post("/", async (req, res) => {
+  const { image } = req.body;
+
+  // Upload
+  const uploadedImage = await cloudinary.uploader.upload(image, {
+      upload_preset: "unsigned_upload",
+      public_id: "avatar",
+      allowed_formats: ["png", "jpg", "jpeg", "svg", "ico", "jfif", "webp"]
+    },
+    function(error, result) {
+      if (error) {
+        console.log(error);
+      }
+      console.log(result);
+    }
+  );
+
+  try {
+    res.json(uploadedImage);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
--- frontend/pages/Upload.js
+++ frontend/pages/Upload.js
@@ -1,44 +1,69 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import axios from "axios"
 
 const Upload = () => {
   const [file, setFile] = useState("");
+  const [image, setImage] = useState("");
+
+  function previewFiles(file) {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onloadend = () => {
+      console.log(image);
+      setImage(reader.result);
+    };
+  }
 
   const handleChange = (e) => {
     const file = e.target.files[0];
-    console.log(file)
-    // setFile(e.target.files);
-  }
+    setFile(file);
+    previewFiles(file);
+  };
 
-  const handleSubmit = () => {
-    // console.log(e.target.files);
-    // setFile(e.target.files);
-  }
+  const handleSubmit = async() => {
+    e.preventDefault();
+    const result = await axios.post("http://localhost:8001", {
+        image: image
+    })
+    try{
+console.log(result.data)
+    }catch(err){
+        console.log(err);
+    }
+  };
 
   return (
-    <div className="flex justify-center items-center mt-10">
-      <form onSubmit={handleSubmit} className="w-1/2">
-        <div className="flex flex-col mb-4">
-          <label htmlFor="fileInput" className="mb-2 font-bold text-lg text-gray-900">
-            Upload Images Here
-          </label>
-          <input
-            type="file"
-            id="fileInput"
-            onChange={handleChange}
-            required
-            accept="image/png, image/jpeg, image/jpg. image/jiff"
-            className="border rounded-lg py-2 px-3"
-          />
-        </div>
-        <button
-          type="submit"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-        >
-          Submit
-        </button>
-      </form>
-    </div>
-  )
-}
+    <>
+      <div className="flex justify-center items-center mt-10">
+        <form onSubmit={handleSubmit} className="w-1/2">
+          <div className="flex flex-col mb-4">
+            <label
+              htmlFor="fileInput"
+              className="mb-2 font-bold text-lg text-gray-900"
+            >
+              Upload Images Here
+            </label>
+            <input
+              type="file"
+              id="fileInput"
+              onChange={handleChange}
+              required
+              accept="image/png, image/jpeg, image/jpg. image/jiff"
+              className="border rounded-lg py-2 px-3"
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+      <img className="flex justify-center items-center p-8 cursor-pointer max-w-2xl"src={image} alt="" />
+    </>
+  );
+};
 
-export default Upload
+export default Upload;
