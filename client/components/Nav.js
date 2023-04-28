7fb401fe7fb3023a776ac9a8f8a2b6bd11f6b50d
danielknight261 <100095189+danielknight261@users.noreply.github.com>
2023-04-28 15:01:01 +0100

    complete overhaul new nav complete

---
+++ client/components/Nav.js
@@ -0,0 +1,76 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div>
+      <header className="p-4 flex justify-between">
+        {/* Logo Container*/}
+        <a href="" className="flex items-center text-red-400">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-8 h-8"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z"
+            />
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z"
+            />
+          </svg>
+          <span className="font-bold text-xl">Pond</span>
+        </a>
+
+        {/* Search Container*/}
+        <div className="flex border border-grey-400 rounded-full py-2 px-10 shadow-md shadow-gray-300">
+          <div>Quick Search</div>
+          <button className="flex justify-end bg-red-400 text-white rounded-full p-1 ml-20">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="w-5 h-5 "
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+              />
+            </svg>
+          </button>
+        </div>
+
+        {/* Login/Upload/Container */}
+        <div className="flex gap-2 py-2 p-4 border border-gray-400 rounded-full ">
+          <div className="">Upload</div>
+          <div className="border-l border-gray-300"></div>
+          <div className="bg-gray-400 rounded-full text-white border border-gray-400">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Nav;
