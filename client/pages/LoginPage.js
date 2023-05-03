2a4e177ef8109c87ae04354b9f230612f69da75a
danielknight261 <100095189+danielknight261@users.noreply.github.com>
2023-05-03 21:50:53 +0100

    continue to work on bcrypt login, slight challenge but huge progress made fun :)

--- client/pages/LoginPage.js
+++ client/pages/LoginPage.js
@@ -1,14 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link';
-import Register from './RegisterPage'
+import axios from 'axios';
 
 const LoginPage = () => {
+
+  const [email, setEmail] =useState('');
+  const [password, setPassword] = useState ('');
+  async function handleLoginSubmit(ev){
+    ev.preventDefault();
+    try{
+      axios.post("http://localhost:4000/login", { email, password });
+
+    alert('Login Successful')
+    }catch (e) {
+      alert('Login failed')
+    }
+  }
+
   return (
     <div className="mt-4 ">
     <h1 className="text-4xl text-center mb-4">Login</h1>
-    <form className="max-w-md mx-auto">
-      <input className="p-2" type="email" placeholder="your@email.com" />
-      <input className="p-2" type="password" placeholder="password" />
+    <form className="max-w-md mx-auto" onSubmit={handleLoginSubmit}>
+
+      <input 
+      className="p-2" 
+      type="email" 
+      placeholder="your@email.com" 
+      value={email}
+      onChange={ev => setEmail(ev.target.value)}
+      />
+      
+      <input 
+      className="p-2" 
+      type="password" 
+      placeholder="password"
+      value={password}
+      onChange={ev => setPassword(ev.target.value)} 
+      />
+
       <button className="bg-red-400 p-2 w-full text-white rounded-2xl">Login</button>
       
       <div className="text-center py-2 text-gray-500">Don't have an account yet ? <Link href="RegisterPage" className="underline text-black">Register now</Link></div>
